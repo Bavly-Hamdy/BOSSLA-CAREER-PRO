d883cb192346627f7bba3378801584b676ee8603
Bavly Hamdy <100946403+Bavly-Hamdy@users.noreply.github.com>
2026-07-24 22:25:07 +0300

    Updates

--- _next/static/pZ2Mz-854NN4GAG1e1RQp/_buildManifest.js
+++ _next/static/pZ2Mz-854NN4GAG1e1RQp/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/BOSSLA-CAREER-PRO//_next/:path+",
-        "destination": "/BOSSLA-CAREER-PRO/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
